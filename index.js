0a6e4870ce41bb7942303cf84a73042f6c142b24
Sobin Johnson <sobinjohnson03@gmail.com>
2024-08-02 18:52:33 +0530

    Cors fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-app.use(cors());
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/bfhl', (req, res) => {
   const { data } = req.body;
